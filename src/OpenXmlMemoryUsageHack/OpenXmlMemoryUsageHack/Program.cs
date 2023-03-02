0455773f7560e7f0b2faf0143406c98c2eabf8ea
pre-alpha-final <jacek.mowinski@gmail.com>
2023-03-02 19:35:26 +0100

    Data mock

--- src/OpenXmlMemoryUsageHack/OpenXmlMemoryUsageHack/Program.cs
+++ src/OpenXmlMemoryUsageHack/OpenXmlMemoryUsageHack/Program.cs
@@ -2,8 +2,25 @@
 
 internal class Program
 {
+    private const int RowCount = 100000;
+    private const int CellCount = 10;
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var data = MockData().ToList();
+    }
+
+    private static IEnumerable<List<string>> MockData()
+    {
+        for (int i = 0; i < RowCount; i++)
+        {
+            var row = new List<string>();
+            for (var j = 0; j < CellCount; j++)
+            {
+                row.Add(Guid.NewGuid().ToString());
+            }
+
+            yield return row;
+        }
     }
 }
