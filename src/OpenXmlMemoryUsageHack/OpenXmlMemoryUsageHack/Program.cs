a07d8d35f40c1f1e79213973bbb5822c85fa2a5d
pre-alpha-final <jacek.mowinski@gmail.com>
2023-03-02 21:00:47 +0100

    Better illustration of what actually changes

--- src/OpenXmlMemoryUsageHack/OpenXmlMemoryUsageHack/Program.cs
+++ src/OpenXmlMemoryUsageHack/OpenXmlMemoryUsageHack/Program.cs
@@ -11,21 +11,51 @@ internal class Program
 
     private static void Main(string[] args)
     {
-        //StandardSave();
-        HackySave();
+        // Standard save
+        //SaveXml("standard.xlsx", StandardRowGeneration);
+
+        // Hacky save
+        //SaveXml("hacky.xlsx", HackyRowGeneration);
 
         Console.WriteLine("Done");
         Console.ReadKey();
     }
 
-    /*
-     * Standard save using example from https://learn.microsoft.com/en-us/dotnet/api/documentformat.openxml.spreadsheet.cellvalue?view=openxml-2.8.1
-     * Modified just enough to work with provided data
-     */
-    private static void StandardSave()
+    private static Row StandardRowGeneration(uint rowIndex, List<string> row)
     {
-        var fileName = @"standard.xlsx";
-        var spreadsheetDocument = SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook);
+        var openXmlRow = new Row() { RowIndex = rowIndex };
+        for (var i = 0; i < CellCount; i++)
+        {
+            var openXmlCell = new Cell()
+            {
+                DataType = CellValues.String,
+                CellValue = new CellValue(row[i])
+            };
+            openXmlRow.Append(openXmlCell);
+        }
+        Console.SetCursorPosition(0, 0);
+        Console.Write($"Row generation {rowIndex * 100 / RowCount} %");
+
+        return openXmlRow;
+    }
+
+    private static Row HackyRowGeneration(uint rowIndex, List<string> row)
+    {
+        var openXmlRow = new RowProxy() { RowIndex = rowIndex };
+        for (var i = 0; i < CellCount; i++)
+        {
+            openXmlRow.AppendCellValue(row[i]);
+        }
+        Console.SetCursorPosition(0, 0);
+        Console.Write($"Row generation {rowIndex * 100 / RowCount} %");
+
+        return openXmlRow;
+    }
+
+    // Modified save using example from https://learn.microsoft.com/en-us/dotnet/api/documentformat.openxml.spreadsheet.cellvalue?view=openxml-2.8.1
+    private static void SaveXml(string filename, Func<uint, List<string>, OpenXmlElement> generateRow)
+    {
+        var spreadsheetDocument = SpreadsheetDocument.Create(filename, SpreadsheetDocumentType.Workbook);
 
         // Add a WorkbookPart to the document.
         var workbookpart = spreadsheetDocument.AddWorkbookPart();
@@ -53,76 +83,20 @@ internal class Program
         uint rowIndex = 1;
         foreach (var row in GetData())
         {
-            var openXmlRow = new Row() { RowIndex = rowIndex++ };
-            for (var i = 0; i < CellCount; i++)
-            {
-                var openXmlCell = new Cell()
-                {
-                    DataType = CellValues.String,
-                    CellValue = new CellValue(row[i])
-                };
-                openXmlRow.Append(openXmlCell);
-            }
+            var openXmlRow = generateRow(rowIndex++, row);
             sheetData.Append(openXmlRow);
         }
 
         worksheet.Append(sheetData);
         worksheetPart.Worksheet = worksheet;
+        Console.WriteLine("\nSaving file");
         workbookpart.Workbook.Save();
 
         // Close the document.
         spreadsheetDocument.Close();
     }
 
-    private static void HackySave()
-    {
-        var fileName = @"hacky.xlsx";
-        var spreadsheetDocument = SpreadsheetDocument.Create(fileName, SpreadsheetDocumentType.Workbook);
-
-        // Add a WorkbookPart to the document.
-        var workbookpart = spreadsheetDocument.AddWorkbookPart();
-        workbookpart.Workbook = new Workbook();
-
-        // Add a WorksheetPart to the WorkbookPart.
-        var worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
-        worksheetPart.Worksheet = new Worksheet(new SheetData());
-
-        // Add Sheets to the Workbook.
-        var sheets = spreadsheetDocument.WorkbookPart.Workbook.AppendChild(new Sheets());
-
-        // Append a new worksheet and associate it with the workbook.
-        var sheet = new Sheet()
-        {
-            Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
-            SheetId = 1,
-            Name = "mySheet"
-        };
-
-        sheets.Append(sheet);
-        var worksheet = new Worksheet();
-        var sheetData = new SheetData();
-
-        uint rowIndex = 1;
-        foreach (var row in GetData())
-        {
-            var openXmlRowProxy = new RowProxy() { RowIndex = rowIndex++ };
-            for (var i = 0; i < CellCount; i++)
-            {
-                openXmlRowProxy.AppendCellValue(row[i]);
-            }
-            sheetData.Append(openXmlRowProxy);
-        }
-
-        worksheet.Append(sheetData);
-        worksheetPart.Worksheet = worksheet;
-        workbookpart.Workbook.Save();
-
-        // Close the document.
-        spreadsheetDocument.Close();
-    }
-
-
-    // Return a string x string table filled with guids (to have some non repeating data)
+    // Return a string table filled with guids (to have some non repeating data)
     private static IEnumerable<List<string>> GetData()
     {
         for (int i = 0; i < RowCount; i++)
