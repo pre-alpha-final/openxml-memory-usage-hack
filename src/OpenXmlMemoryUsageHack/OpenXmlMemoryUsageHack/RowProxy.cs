03d84d3241777984760af3d77759ff9bc037494e
pre-alpha-final <jacek.mowinski@gmail.com>
2023-03-09 17:04:03 +0100

    Support for additional data types

--- src/OpenXmlMemoryUsageHack/OpenXmlMemoryUsageHack/RowProxy.cs
+++ src/OpenXmlMemoryUsageHack/OpenXmlMemoryUsageHack/RowProxy.cs
@@ -5,25 +5,45 @@ namespace OpenXmlMemoryUsageHack;
 
 internal class RowProxy : Row
 {
-    private readonly List<string> _cellValues = new();
+    private readonly List<object> _cellValues = new();
 
     private List<OpenXmlElement> Children => MapToCell(_cellValues);
     public override OpenXmlElementList ChildElements => new ElementListProxy(() => Children);
     public override OpenXmlElement FirstChild => Children.FirstOrDefault();
     public override bool HasChildren => Children.Any();
 
-    public void AppendCellValue(string cellValue)
+    public void AppendCellValue(object cellValue)
     {
         _cellValues.Add(cellValue);
     }
 
-    private static List<OpenXmlElement> MapToCell(IEnumerable<string> cellValues)
+    private static List<OpenXmlElement> MapToCell(IEnumerable<object> cellValues)
     {
-        return cellValues.Select(e => (OpenXmlElement)new Cell
+        return cellValues.Select(MapCell).ToList();
+    }
+
+    private static OpenXmlElement MapCell(object cellValue)
+    {
+        switch (cellValue.GetType().Name)
         {
-            DataType = CellValues.String,
-            CellValue = new CellValue(e)
-        }).ToList();
+            case "String":
+                return new Cell { DataType = CellValues.String, CellValue = new CellValue((string)cellValue) };
+            case "Boolean":
+                return new Cell { DataType = CellValues.Boolean, CellValue = new CellValue((bool)cellValue) };
+            case "DateTime":
+                return new Cell { DataType = CellValues.Date, CellValue = new CellValue((DateTime)cellValue), StyleIndex = 1 };
+            case "Int32":
+                return new Cell { DataType = CellValues.Number, CellValue = new CellValue((int)cellValue) };
+            case "Single":
+                return new Cell { DataType = CellValues.Number, CellValue = new CellValue((float)cellValue) };
+            case "Double":
+                return new Cell { DataType = CellValues.Number, CellValue = new CellValue((double)cellValue) };
+            case "Decimal":
+                return new Cell { DataType = CellValues.Number, CellValue = new CellValue((decimal)cellValue) };
+            default:
+                return new Cell { DataType = CellValues.String, CellValue = new CellValue(cellValue?.ToString()) };
+                //throw new ArgumentOutOfRangeException($"{cellValue.GetType().Name} type not supported");
+        }
     }
 
     public class ElementListProxy : OpenXmlElementList
